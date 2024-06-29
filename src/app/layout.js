f1d0b96ceab573f000dac037606996040276dd08
HillaryG1 <hillgilbert7gmail.com>
2024-06-29 19:37:46 -0400

    First Commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,27 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Navbar from "@/components/navbar/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/footer/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+  title: "Heaven's Perspective",
+  description: "Exploring Faith, Scripture, and Divine Insights",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <div className="wrapper">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
