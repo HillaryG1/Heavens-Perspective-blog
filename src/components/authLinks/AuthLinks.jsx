f27bc7e147e5ee29a0f1876f06870ba6e43ec2eb
HillaryG1 <hillgilbert7gmail.com>
2024-06-29 20:16:20 -0400

    Implement Navbar

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,9 @@
+import styles from "./authlinks.module.css";
+
+const AuthLinks = () => {
+  //temporary
+  const status = "notauthenticated";
+  return <div className="styles.container">AuthLinks</div>;
+};
+
+export default AuthLinks;
