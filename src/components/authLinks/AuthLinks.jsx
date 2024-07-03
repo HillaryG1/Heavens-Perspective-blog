8aae2e517520f5bf360b5597f21de5a91544b918
HillaryG1 <hillgilbert7gmail.com>
2024-07-02 21:57:34 -0400

    Implement Dark/Light Theme

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
 import styles from "./authlinks.module.css";
 
 const AuthLinks = () => {
   //temporary
   const status = "notauthenticated";
-  return <div className="styles.container">AuthLinks</div>;
+  return (
+    <>
+      {status === "notauthenticated" ? (
+        <Link href="/login">Login</Link>
+      ) : (
+        <>
+          <Link href="/write">Write</Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLinks;
