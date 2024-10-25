37eedaaafe04a05a63c9179ad648adad46761c01
HillaryG1 <hillgilbert7gmail.com>
2024-10-25 13:03:18 -0400

    Add comments section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/pro.png" alt="Hillary Gilbert" width={70} height={70} />
+          <h1 className={styles.logoText}>Heaven&#39;s Perspective</h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+          necessitatibus similique aspernatur obcaecati veritatis. Aperiam cum
+          porro sequi, totam minima consequuntur, aspernatur deleniti vero
+          repellendus dorales.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="" width={18} height={18} />
+          <Image src="/instagram.png" alt="" width={18} height={18} />
+          <Image src="/tiktok.png" alt="" width={18} height={18} />
+          <Image src="/youtube.png" alt="" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Christ</Link>
+          <Link href="/">Faith</Link>
+          <Link href="/">Wisdom</Link>
+          <Link href="/">Stories</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
